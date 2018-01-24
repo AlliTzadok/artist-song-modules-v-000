54ac60a4f8143455a64cbaeca5a2df586db5364c
Alli Tzadok <allisontzadok@gmail.com>
2018-01-24 20:14:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,6 @@ class Song
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
 
-
-
   @@songs = []
 
   def initialize
@@ -21,15 +19,12 @@ class Song
   #   @@songs.detect{|a| a.name == name}
   # end
 
-  def artist=(artist)
-    @artist = artist
-  end
+def artist=(artist)
+@artist = artist
+artist.add_song(self) unless artist.songs.include?(self)
+end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.all
-    @@songs
-  end
+def self.all
+@@songs
+end
 end
