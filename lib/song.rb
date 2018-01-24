9cf8f0bca086412e46cb14078e9ee1de67c8ed83
Alli Tzadok <allisontzadok@gmail.com>
2018-01-24 20:15:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,18 +5,18 @@ class Song
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
-
+  
   attr_accessor :name
   attr_reader :artist
-
+  
   @@songs = []
-
-def artist=(artist)
-@artist = artist
-artist.add_song(self) unless artist.songs.include?(self)
-end
-
-def self.all
-@@songs
-end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
+  
+  def self.all
+    @@songs
+  end
 end
